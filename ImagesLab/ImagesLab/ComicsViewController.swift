6abe181f7af91d6e839915ac19ec675b70a87280
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-12-08 16:02:19 -0500

    updated UI

---
+++ ImagesLab/ImagesLab/ComicsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ImagesLab
+//
+//  Created by Oscar Victoria Gonzalez  on 12/7/19.
+//  Copyright © 2019 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class ComicsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var stepperLabel: UIStepper!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    @IBAction func mostRecentButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func randomButton(_ sender: UIButton) {
+    }
+    
+
+}
+
